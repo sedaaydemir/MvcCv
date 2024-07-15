75c105aeccfd520d9927f0949c178b443777f97d
Seda Aydemir <seda.aydemir7@gmail.com>
2024-07-15 19:52:35 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;//modeli klasoru cağır
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimler()
+        {
+            var egitimler = db.TblEgitimler.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yetenekler()
+        {
+            var yetenekler = db.TblYetenekler.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobiler()
+        {
+            var hobiler = db.TblHobiler.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.TblSertifikalar.ToList();
+            return PartialView(sertifikalar);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(TblIletisim t)
+        {
+            t.Tarih=DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblIletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
