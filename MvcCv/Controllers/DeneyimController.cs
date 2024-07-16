0a9debbef768bc7ce4999c51897869ef3463fa6c
Seda Aydemir <seda.aydemir7@gmail.com>
2024-07-16 23:31:55 +0300

    ---

---
+++ MvcCv/Controllers/DeneyimController.cs
@@ -0,0 +1,21 @@
+﻿using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+
+        DeneyimRepository repo =new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var degerler = repo.List();
+            return View(degerler);
+        }
+    }
+}
